58f6e51efe7517d2813c638271df12e3511f57b7
seeds-r-kato <r.kato@seeds-std.co.jp>
2024-11-13 12:13:53 +0900

    error log

--- upload-file-over-time.php
+++ upload-file-over-time.php
@@ -1,14 +1,16 @@
 <?php
 // ファイルアップロード用
-// curl -X POST -F "file=@/path/to/large-file" https://your-cloudfront-domain.com/upload-test.php
+// ec2 curl -X POST -F "file=@/path/to/large-file" http://ec2-52-199-123-36.ap-northeast-1.compute.amazonaws.com/cloudfront-test/upload-file-over-time.php
+// cloudfront curl -X POST -F "file=@/path/to/large-file" https://d1zg773tvuk57c.cloudfront.net/cloudfront-test/upload-file-over-time.php
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
     // アップロードされたファイルを保存
     $uploadDir = '/var/www/uploads/';
     $uploadFile = $uploadDir . basename($_FILES['file']['name']);
     move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile);
-    sleep(65); // アップロード処理に65秒の遅延
+    // sleep(65); // アップロード処理に65秒の遅延
     echo "Upload completed";
 } else {
+    error_log("Request method is not POST: " . $_SERVER['REQUEST_METHOD'], 3, $logFile);
     echo "No file uploaded";
 }
 ?>
