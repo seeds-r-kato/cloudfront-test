0b2ff7604fd7169c1592f274dcc6d3fb1f2c0460
seeds-r-kato <r.kato@seeds-std.co.jp>
2024-11-13 12:24:23 +0900

    log

--- upload-file-over-time.php
+++ upload-file-over-time.php
@@ -2,7 +2,7 @@
 // ファイルアップロード用
 // ec2 curl -X POST -F "file=@/path/to/large-file" http://ec2-52-199-123-36.ap-northeast-1.compute.amazonaws.com/cloudfront-test/upload-file-over-time.php
 // cloudfront curl -X POST -F "file=@/path/to/large-file" https://d1zg773tvuk57c.cloudfront.net/cloudfront-test/upload-file-over-time.php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
     // アップロードされたファイルを保存
     $uploadDir = '/var/www/uploads/';
     $uploadFile = $uploadDir . basename($_FILES['file']['name']);
@@ -11,6 +11,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
     echo "Upload completed";
 } else {
     echo ("Request method is not POST: " . $_SERVER['REQUEST_METHOD']);
+    echo "<br>";
+    echo ("Uploaded file: " . $_FILES['file']);
+    echo "<br>";
     echo "No file uploaded";
 }
 ?>
