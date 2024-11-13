321db08bda84651f8b86155d5fca0c321f7cde8a
seeds-r-kato <r.kato@seeds-std.co.jp>
2024-11-13 12:53:50 +0900

    upload folder error

--- upload-file-over-time.php
+++ upload-file-over-time.php
@@ -4,6 +4,10 @@
 // cloudfront curl -X POST -F "file=@/path/to/large-file" https://d1zg773tvuk57c.cloudfront.net/cloudfront-test/upload-file-over-time.php
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file'])) {
     // アップロードされたファイルを保存
+    if ($_FILES['file']['error'] !== UPLOAD_ERR_OK) {
+        echo "Failed to upload file with error code: " . $_FILES['file']['error'];
+        exit;
+    }
     $uploadDir = '/var/www/html/uploads/';
     $uploadFile = $uploadDir . basename($_FILES['file']['name']);
     move_uploaded_file($_FILES['file']['tmp_name'], $uploadFile);
