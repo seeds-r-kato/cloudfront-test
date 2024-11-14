d260185427141c9537e270505568525dc0c8c204
seeds-r-kato <r.kato@seeds-std.co.jp>
2024-11-14 10:35:59 +0900

    3分

--- response-every-30sec.php
+++ response-every-30sec.php
@@ -1,14 +1,14 @@
 <?php
 // 無限ループで30秒ごとにレスポンス
-// 2分経ったらタイムアウト
+// 3分経ったらタイムアウト
 $time = 0;
 while (true) {
-    echo "response at " . date("H:i:s") . "\n";
+    echo "response at " . $time . " seconds\n";
     ob_flush();
     flush();
     sleep(30);
     $time += 30;
-    if ($time > 120) {
+    if ($time > 180) {
         break;
     }
 }
