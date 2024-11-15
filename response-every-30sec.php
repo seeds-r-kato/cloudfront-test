a52738d643fbe7302eec7d509f4981e3390f651a
seeds-r-kato <r.kato@seeds-std.co.jp>
2024-11-15 10:12:50 +0900

    61秒の遅延を挟む

--- response-every-30sec.php
+++ response-every-30sec.php
@@ -1,6 +1,6 @@
 <?php
 // 無限ループで30秒ごとにレスポンス
-// 2分経ったら終了
+// 2分経ったら、61秒間の遅延を挟む
 $time = 0;
 while (true) {
     echo "response at " . $time . " seconds\n";
@@ -8,7 +8,9 @@ while (true) {
     flush();
     sleep(30);
     $time += 30;
-    if ($time > 120) {
+    if ($time === 120) {
+        sleep(61);
+        echo "Response after 61 seconds";
         break;
     }
 }
